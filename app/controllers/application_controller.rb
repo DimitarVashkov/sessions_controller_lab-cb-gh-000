643f719de918e7415aad2ca4449b4ef0c3e2a91e
Dimitar Vashkov <dimitarvashkov@icloud.com>
2018-04-16 01:59:50 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,7 +15,7 @@ class ApplicationController < ActionController::Base
   private
 
   def auth
-     redirect_to '/sessions/new' unless current_user
+     redirect_to :controller => 'sessions', :action => 'new'  unless current_user
   end
 
 end
