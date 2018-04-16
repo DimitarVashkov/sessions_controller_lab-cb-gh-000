6f53a14f2afe3b0f86773c7cb73c6471ed6d43cb
Dimitar Vashkov <dimitarvashkov@icloud.com>
2018-04-16 02:02:32 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,12 +3,15 @@ class SessionsController < ApplicationController
   end
 
   def create
+    return redirect_to(controller: 'sessions',
+                       action: 'new') if !params[:name] || params[:name].empty?
     session[:name] = params[:name]
-    redirect_to '/'
+    redirect_to controller: 'application', action: 'hello'
   end
 
   def destroy
     session.delete :name
+    redirect_to controller: 'application', action: 'hello'
   end
 
 end
